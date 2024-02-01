1885c959728248af95316e473bec0f028d22b553
Wade Armstrong <wade@wadearmstrong.com>
2024-01-31 16:35:47 -0800

    Type improvements; return a random item from a list

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from "react";
 import { ActionPanel, Detail, List, Action } from "@raycast/api";
 
-const placesToWork = [10,11,12]
+const placesToWork = ["10","11","12"]
 const lunchJoints = ["McDonalds", "Burger King", "KFC"]
 const pickLists = [{title: "Places to work", contents: placesToWork}, { title: "Lunch joints", contents: lunchJoints}]
 
 interface ListElement {
   title: string;
-  contents: object;
+  contents: string[];
 }
 
 function ListParent(props: { listElements: ListElement[],
   navigationTitle: string,
   searchBarPlaceholder: string,
-  onSearchTextChange: (searchText: string) => void}
-  ) {
+  onSearchTextChange: (searchText: string) => void
+}) {
   return (
     <List
       onSearchTextChange={props.onSearchTextChange}
@@ -28,13 +28,17 @@ function ListParent(props: { listElements: ListElement[],
           title={list.title}
           actions={
             <ActionPanel>
-              <Action.Push title="Show List" target={<Detail markdown={String(list.contents)}/>} />
+              <Action.Push title="Show List" target={<Detail markdown={randomPick(list.contents)}/>} />
             </ActionPanel>
           }
         />
       ))}
     </List>
   );
+}
+
+function randomPick(list: string[]) {
+  return String(list[Math.floor(Math.random() * list.length)]);
 }
 
 
